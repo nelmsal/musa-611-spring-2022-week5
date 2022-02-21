8d9567a7fe4374688382580906c8f3a7b3921a17
Alex Nelms <nelmsal@gmail.com>
2022-02-20 22:11:50 -0500

    start exercise

--- exercise/school-explorer-redux/index.js
+++ exercise/school-explorer-redux/index.js
@@ -22,6 +22,116 @@ const zipCodeSelect = document.querySelector('#zip-code-select');
 ==================== */
 
 let showSchoolInfo = (school, marker) => {
+  const ulcs = school['UCLS Code'];
+  const response = fetch('../../data/demographics/${ulcs}.json');
 }
 
 /* PASTE YOUR WEEK4 EXERCISE CODE HERE */
+
+let showPopup = (marker, school) => {
+  let dataFileName = `../data/demographics/${school['UCLS Code']}.json`;
+  const response = fetch(dataFileName)
+    .then(response => response.json())
+    .then(
+      data => {
+        console.log(data);
+    });
+
+
+  let schoolPub = school['Publication Name'];
+  let popInfo = `<h3>${schoolPub}</h3>`;
+  marker.bindPopup(popInfo).openPopup();
+};
+
+let updateSchoolMarkers = (schoolsToShow) => {
+  schoolLayer.clearLayers();
+  schoolsToShow.forEach((school) => {
+    const name = school['Publication Name'];
+    const [lat, lng] = school['GPS Location'].split(',');
+    const marker = L.marker([parseFloat(lat), parseFloat(lng)]).bindTooltip(name);
+
+    schoolLayer.addLayer(marker);
+
+    marker.addEventListener('click', () => {
+      showPopup(marker, school);
+    });
+
+  });
+};
+
+let updateSchoolList = (schoolsToShow) => {
+  let school_arr = []
+  schoolsToShow.forEach(school => {
+    school_arr.push(school['Publication Name']);
+  })
+
+  school_arr.sort();
+  let schoolList_container = document.getElementById('school-list');
+  school_arr.forEach(name => {
+    schoolList_container.appendChild(htmlToElement(`<li>${name}</li>`));
+  });
+};
+
+let initializeZipCodeChoices = () => {
+  let zip_arr = []
+  schools.forEach(school => {
+    let zip = school['Zip Code'].split('-',1)[0];
+    if (!zip_arr.includes(zip)) {
+      zip_arr.push(zip)
+    };
+  })
+  zip_arr.sort();
+  let zipContainer = document.getElementById('zip-code-select');
+  zip_arr.forEach(zip => {
+    zipContainer.appendChild(htmlToElement(`<option>${zip}</option>`));
+  });
+};
+
+let filteredSchools = () => {
+  let current_zip = zipCodeSelect.value
+  let current_grade = gradeLevelSelect.value
+  console.log(current_grade)
+
+
+  let isGrade = (school) => {
+    if (current_grade === '') {
+      return school
+    }
+    else if ( school[current_grade] === '1') {
+      return school
+    }
+  };
+  let isZip = (school) => {
+    if (current_zip === '') {
+      return school
+    } else if (school['Zip Code'].split('-')[0] === current_zip) {
+      return school
+    }
+  };
+
+  return schools.filter(isZip).filter(isGrade);
+};
+
+
+/*
+
+No need to edit anything below this line ... though feel free.
+
+*/
+
+// The handleSelectChange function is an event listener that will be used to
+// update the displayed schools when one of the select filters is changed.
+let handleSelectChange = () => {
+  const schoolsToShow = filteredSchools() || [];
+  updateSchoolMarkers(schoolsToShow);
+  updateSchoolList(schoolsToShow);
+};
+
+gradeLevelSelect.addEventListener('change', handleSelectChange);
+zipCodeSelect.addEventListener('change', handleSelectChange);
+
+// The code below will be run when this script first loads. Think of it as the
+// initialization step for the web page.
+initializeZipCodeChoices();
+updateSchoolMarkers(schools);
+updateSchoolList(schools);
